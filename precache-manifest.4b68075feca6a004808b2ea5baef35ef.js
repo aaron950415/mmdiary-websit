67965f5de99d2019f8993905e1a009a2df9409f4
Aaron <aaron950415@qq.com>
2021-07-29 21:32:10 +1000

    deploy

---
+++ precache-manifest.4b68075feca6a004808b2ea5baef35ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51523c7223eca2d43bb8",
+    "url": "/mmdiary-websit/css/app.aa6a4b42.css"
+  },
+  {
+    "revision": "f5676609ec4bc5e7bb924dd249aadfdd",
+    "url": "/mmdiary-websit/index.html"
+  },
+  {
+    "revision": "51523c7223eca2d43bb8",
+    "url": "/mmdiary-websit/js/app.e299f30a.js"
+  },
+  {
+    "revision": "a92b0f8845ee3ff6d5d3",
+    "url": "/mmdiary-websit/js/chunk-vendors.779efdaf.js"
+  },
+  {
+    "revision": "50031c9470418a93ee92e62e8be77d17",
+    "url": "/mmdiary-websit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mmdiary-websit/robots.txt"
+  }
+]);
